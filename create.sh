7124647144f0f77035ec826ee29ef17e5f88743b
S Martin <stephenwmartin@protonmail.ch>
2017-08-28 10:40:19 -0500

    Blank chapters

---
+++ create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#script to generate tree folder of a boook (chapters only), will create  01-, 02-, ... XX folders with a 01-capterxx.markdown file
+
+for i in {20..200}
+do
+   mkdir 0$i- && touch 01-chapter0$i.markdown && mv 01-chapter0$i.markdown 0$i-/
+done
